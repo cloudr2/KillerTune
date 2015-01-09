55a0f391af8af51c08fdff82c4e10728f69ef636
cloudr2 <alejandro.popovsky@gmail.com>
2015-01-09 17:37:34 -0300

    empece a usar uGUI

---
+++ src/Assets/ScaleFontSize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleFontSize : MonoBehaviour {
+
+	private static int nativeWidth = 720;
+	private static int nativeFont = 37;
+	private int currentRes {get{return Screen.width;}}
+
+
+	void Start () {
+	
+	}
+
+}
